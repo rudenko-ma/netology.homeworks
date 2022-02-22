3fc791f73f46355cb87319fb13f48055348df85a
rudenko.m.a <rudenko.m.a@mail.ru>
2022-02-23 00:22:15 +1000

    Question 4 fix

--- 04-script-02-py/q4.py
+++ 04-script-02-py/q4.py
@@ -7,12 +7,18 @@ import yaml
 # путь к файлу в котором хранится последнее состояние
 state_file = 'state.yaml'
 
-# если файл существует и доступен на чтение и запись...
+# если файл существует...
 state_file = os.path.abspath(state_file)
-if os.path.isfile(state_file) and os.access(state_file, os.R_OK) and os.access(state_file, os.W_OK):
-    # читаем последнее состояние
-    with open(state_file) as f:
-        urls = yaml.safe_load(f)
+if os.path.isfile(state_file):
+    # проверяем права
+    if os.access(state_file, os.R_OK) and os.access(state_file, os.W_OK):
+        # читаем последнее состояние
+        with open(state_file) as f:
+            urls = yaml.safe_load(f)
+    else: 
+        # для работы не хватает прав
+        print(f"[ERROR] Файл '{state_file}' должен быть доступен на чтение и запись!')
+        sys.exit(1)
 else:
     # иначе - инициализируем словарь имен без адресов
     urls = {'drive.google.com':'0.0.0.0', 'mail.google.com':'0.0.0.0', 'google.com':'0.0.0.0'}
